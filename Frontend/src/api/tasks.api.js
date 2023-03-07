7ba8a8e2a167e9aaaf61f33285b84296e6d1c649
Juan David Perez <juandevperez@gmail.com>
2023-03-06 21:49:56 -0300

    sdfsdfsdfd

--- Frontend/src/api/tasks.api.js
+++ Frontend/src/api/tasks.api.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
-const PORT = process.env.PORT || 8181;
-const URL = process.env.RAILWAY_STATIC_URL || 'http://localhost';
+// const PORT = process.env.PORT || 8181;
+// const URL = process.env.RAILWAY_STATIC_URL || 'http://localhost';
 
 export const getAll = async () => 
-    await axios.get(`${URL}:${PORT}/tasks`);
+    await axios.get(`https://deploy-tasks-production.up.railway.app/tasks`);
 
 export const getOne = async id => 
-    await axios.get(`${URL}:${PORT}/tasks/${id}`);
+    await axios.get(`https://deploy-tasks-production.up.railway.app/tasks/${id}`);
 
 export const createTask = async data => 
-    await axios.post(`${URL}:${PORT}/tasks`, data);
+    await axios.post(`https://deploy-tasks-production.up.railway.app/tasks`, data);
 
 export const updateTask = async (id, data) => 
-    await axios.put(`${URL}:${PORT}/tasks/${id}`, data);
+    await axios.put(`https://deploy-tasks-production.up.railway.app/tasks/${id}`, data);
 
 export const checkTaskDone = async (id, done) => 
-    await axios.put(`${URL}:${PORT}/tasks/${id}`, { done } );
+    await axios.put(`https://deploy-tasks-production.up.railway.app/tasks/${id}`, { done } );
 
 export const deleteTask = async id => 
-    await axios.delete(`${URL}:${PORT}/tasks/${id}`);
+    await axios.delete(`https://deploy-tasks-production.up.railway.app/tasks/${id}`);
